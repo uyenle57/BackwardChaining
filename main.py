be83fc671e68a95e5f4b20817d09d249876ee785
Uyen Le <tle004@gold.ac.uk>
2017-11-28 11:39:11 +0000

    Added code skeleton

---
+++ main.py
@@ -0,0 +1,44 @@
+import textwrap
+
+
+def main():
+
+    print(textwrap.dedent("""
+    ================================================================
+                      ARTIFICIAL INTELLIGENCE
+            Coursework 2: Backward Chaining Rule Based System
+    
+                            Uyen Le
+                        tle004@gold.ac.uk
+    ================================================================
+    """))
+
+
+
+
+main()
+
+# KNOWLEDGE BASE
+# contains Rules and Facts
+
+# WORKING MEMORY
+# contains Facts
+
+# INFERENCE ENGINE
+# Match -> Select -> Act
+
+# Match a goal with a consequent
+def matchGoal(goal):
+    pass
+
+# Match a subgoal with antecedents from a rule
+def matchSubgoal(subgoal):
+    pass
+
+# Select function
+def select():
+    pass
+
+# Act function
+def act():
+    pass
