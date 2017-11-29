66432ad195562739b71f567169e74f99dff3113d
Uyen Le <tle004@gold.ac.uk>
2017-11-29 13:54:03 +0000

    Implementing Depth-First Search

--- Rule.py
+++ Rule.py
@@ -5,15 +5,15 @@ class Rule():
     antecedents = []
     consequence = ()
 
-    def __init__(self, antecedents, consequence):
+    def __init__(self, antecedents, consequent):
         self.antecedents = antecedents
-        self.consequence = consequence
+        self.consequent = consequent
 
     def get_antecedents(self):
         return self.antecedents
 
-    def get_consequence(self):
-        return self.consequence
+    def get_consequent(self):
+        return self.consequent
 
     def to_string(self):
-        return "IF " + str(self.antecedents) + " \n\tTHEN " + str(self.consequence)
+        return "IF " + str(self.antecedents) + " \n\tTHEN " + str(self.consequent)
